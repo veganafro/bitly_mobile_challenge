c1da298e300ebf4cd3ab60ede9f204de431cf4cf
veganafro <jmuhia80@gmail.com>
2017-03-12 23:28:28 -0400

    first commit

---
+++ bitly-api-app/bitly-api-appTests/bitly_api_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  bitly_api_appTests.swift
+//  bitly-api-appTests
+//
+//  Created by Jeremia Muhia on 3/12/17.
+//  Copyright © 2017 NYU. All rights reserved.
+//
+
+import XCTest
+@testable import bitly_api_app
+
+class bitly_api_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
