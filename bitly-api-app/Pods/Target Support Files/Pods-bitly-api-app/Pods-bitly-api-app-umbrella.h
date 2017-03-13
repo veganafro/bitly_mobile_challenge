9a7746af4060f4d979bd5c9ee23f380214b180ad
veganafro <jmuhia80@gmail.com>
2017-03-13 00:12:14 -0400

    add cocoapods

---
+++ bitly-api-app/Pods/Target Support Files/Pods-bitly-api-app/Pods-bitly-api-app-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_bitly_api_appVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_bitly_api_appVersionString[];
+
---
+++ bitly-api-app/Pods/Target Support Files/Pods-bitly-api-appUITests/Pods-bitly-api-appUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_bitly_api_appUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_bitly_api_appUITestsVersionString[];
+
