838cf861067caa19ce9852547c02382287424762
veganafro <jmuhia80@gmail.com>
2017-03-13 00:19:24 -0400

    start working

--- bitly-api-app/bitly-api-app/AppDelegate.swift
+++ bitly-api-app/bitly-api-app/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BitlySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Bitly.initialize("")
         // Override point for customization after application launch.
         return true
     }
