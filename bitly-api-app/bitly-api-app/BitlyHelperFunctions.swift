65ab16ab477b7c85de1bd59b3a0a7bfe36a13149
veganafro <jmuhia80@gmail.com>
2017-03-14 01:20:13 -0400

    building backend

--- bitly-api-app/bitly-api-app/BitlyHelperFunctions.swift
+++ bitly-api-app/bitly-api-app/BitlyHelperFunctions.swift
@@ -14,7 +14,10 @@ import BitlySDK
     The project is architected in this way to separate UI logic from backend logic.
  */
 class BitlyHelperFunctions: NSObject {
-
+    
+    // we have a global base URL that can be used to reach specific API endpoints
+    static var baseURL = "https://api-ssl.bitly.com/v3/user/"
+    
     /*
         The function below uses the /v3/user/link_save endpoint of the Bitly API to save a long URL
         as a Bitlink in a user's history.
@@ -26,7 +29,7 @@ class BitlyHelperFunctions: NSObject {
             - the title of the Bitlink associated with longURL
      */
     static func linkSave(longURL: String, title: String) {
-    
         
+        var request = URLRequest(url: URL(string: baseURL))
     }
 }
